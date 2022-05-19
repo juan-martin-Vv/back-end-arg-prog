91ddf4fba14f733cee82f184950d519fc8fa8044
juan-martin-Vv <juan.martin.w@gmail.com>
2022-05-19 15:48:51 -0300

    deploy 1.61

--- src/main/java/arg/prog/backend/Controller/PerfilController.java
+++ src/main/java/arg/prog/backend/Controller/PerfilController.java
@@ -3,7 +3,7 @@ package arg.prog.backend.Controller;
 import javax.annotation.security.PermitAll;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
