ea42c56ddb9b326aaab0931c8c6c3b1dc8266cfc
juan-martin-Vv <juan.martin.w@gmail.com>
2022-05-23 16:25:29 -0300

    arreglando cors

--- src/main/java/arg/prog/backend/Configuracion/CorsConfig.java
+++ src/main/java/arg/prog/backend/Configuracion/CorsConfig.java
@@ -1,5 +1,6 @@
 package arg.prog.backend.Configuracion;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,9 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${config.front.url}")
+    private String fronString;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+        registry.addMapping("/**").allowedOrigins(fronString).allowedMethods("*");
         WebMvcConfigurer.super.addCorsMappings(registry);
     }
 
